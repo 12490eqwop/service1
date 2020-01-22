32237363ad6352cd1b2d83ea462b41094ccfeb0e
12490eqwop <dkcktksvkfkd@naver.com>
2020-01-22 21:25:46 +0900

    ddd

---
+++ src/main/java/com/example/service1/controller/Service1Controller.java
@@ -0,0 +1,20 @@
+package com.example.service1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Service1Controller {
+
+    @GetMapping("/")
+    public String service1(){
+        return "==Page1==";
+    }
+
+    @GetMapping("/view")
+    public String service(){
+        return "==Page1==의 service";
+    }
+}
